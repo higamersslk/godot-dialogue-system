9646645a130751b4a5e3196c43ff87d6ee499f67
higamersslk <61049859+higamersslk@users.noreply.github.com>
2026-02-03 20:31:21 -0300

    refactor dialogue manager: removes depedency on DialogueArea2D. Supports better conversations.

--- source/dialogue_system/dialogue_manager.gd
+++ source/dialogue_system/dialogue_manager.gd
@@ -2,67 +2,138 @@ class_name DialogueManager
 extends Node2D
 
 
-signal dialogue_started
-signal dialogue_ended
+signal dialogue_started(topic_id: StringName)
+signal dialogue_ended(topic_id: StringName)
+signal continue_next
+
+@export_group("Dialogue Settings")
+@export var entry_topic: StringName
+@export var dialogue_box_screen_position: Vector2
+@export var dialogue_box_position_offset: Vector2
+@export var one_time_dialogue: bool
+
+@export_group("Resources")
+@export var dialogue_container: DialogueContainer
+@export var dialogue_box_scene: PackedScene
+@export var trigger: DialogueArea2D
+
+var player: Player
+var dialogue_box: DialogueTextBox
+
+var current_topic_id: StringName
+var current_node_index: int = -1
+var current_node: DialogueNode
+
+var _topics_data: Dictionary[StringName, Array]
+var _nodes: Array
+
+var is_chatting: bool
+var _dialogue_finished: bool
+
+static var registered_functions: Array[Callable]
 
 
-var text_box: TextBox
+func _ready() -> void:
+	if not dialogue_container:
+		push_warning("Dialogue Manager: no valid dialogue container.")
+		return
+	if not dialogue_box_scene:
+		push_warning("Dialogue manager: no valid dialogue box.")
+		return
 
-var context: Dictionary
-var dialogue_data: Dictionary[StringName, Array]
-var current_blocks: Array
+	if trigger:
+		trigger.player_trigged_dialogue.connect(_on_dialogue_trigged)
+		trigger.player_left_dialogue_area.connect(end_dialogue)
+	
+	dialogue_box = dialogue_box_scene.instantiate() as DialogueTextBox
+	add_child(dialogue_box)
 
-var current_block_index: int = -1
-var current_dialogue_label: StringName
-
-static var registered_functions: Dictionary[String, Callable]
+	current_topic_id = entry_topic
+	_topics_data = dialogue_container.topics
 
 
-func start_dialogue(dialogue_container: DialogueContainer) -> void:
-	dialogue_data = dialogue_container.dialogues
-	current_blocks = dialogue_data[current_dialogue_label] as Array[DialogueBlock]
+func _input(event: InputEvent) -> void:
+	if not is_chatting: return
+	if not event.is_action_pressed("interact"): return
 
-	if current_blocks.is_empty():
+	if dialogue_box.is_typing:
+		dialogue_box.skip_typing()
+	else:
+		continue_next.emit()
+
+
+func _process_nodes() -> void:
+	current_node_index += 1
+	if current_node_index >= _nodes.size():
 		end_dialogue()
 		return
-	
-	dialogue_started.emit()
-	_instantiate_text_box()
-	_next_block()
+
+	is_chatting = true
+	current_node = _nodes[current_node_index]
+	current_node.finished.connect(_process_nodes, CONNECT_ONE_SHOT)
+	current_node.execute(self)
+
+
+func _on_dialogue_trigged(_player: Player) -> void:
+	player = _player
+	start_dialogue()
+
+
+func start_dialogue(topic_id: StringName = current_topic_id) -> void:
+	if one_time_dialogue and _dialogue_finished: return
+	_dialogue_finished = false
+	switch_topic(topic_id)
+	_process_nodes()
+	dialogue_started.emit(topic_id)
+
+
+func switch_topic(topic_id: StringName) -> void:
+	if one_time_dialogue and _dialogue_finished: return
+	if _topics_data.is_empty() or not _topics_data.has(topic_id):
+		push_warning("Dialogue manager: no dialogue data found.")
+		return
+
+	current_node_index = -1
+	current_topic_id = topic_id
+	_nodes = _topics_data[topic_id]
 
 
 func end_dialogue() -> void:
-	dialogue_ended.emit()
+	if current_node:
+		if current_node.finished.is_connected(_process_nodes):
+			current_node.finished.disconnect(_process_nodes)
+		current_node.cancel(self)
 
-
-func show_text_box() -> void:
-	if not text_box:
-		_instantiate_text_box()
-	text_box.visible = true
+	if dialogue_box.is_typing:
+		dialogue_box.skip_typing()
 	
+	clean_up_signals()
 
-func hide_text_box() -> void:
-	if not text_box: return
-	text_box.visible = false
+	is_chatting = false
+	_dialogue_finished = true
+	player = null
+	current_node = null
+
+	dialogue_box.hide()
+	continue_next.emit()
+	dialogue_ended.emit(current_topic_id)
 
 
-func _next_block() -> void:
-	current_block_index += 1
-	if current_block_index >= current_blocks.size():
-		end_dialogue()
-		return
+func clean_up_signals() -> void:
+	var text_finished: Signal = dialogue_box.text_finished
 	
-	var block: DialogueBlock = current_blocks[current_block_index]
-	block.finished.connect(_next_block, CONNECT_ONE_SHOT)
-	block.execute(self)
+	for connection: Dictionary in text_finished.get_connections():
+		text_finished.disconnect(connection.callable)
+
+	for connection: Dictionary in continue_next.get_connections():
+		continue_next.disconnect(connection.callable)
 
 
-func _instantiate_text_box() -> void:
-	if text_box: return
+static func subscribe(function: Callable) -> void:
+	if registered_functions.has(function): return
+	registered_functions.append(function)
 
-	text_box = context["text_box"].instantiate() as TextBox
-	text_box.visible = false
 
-	var dialogue_area: DialogueArea2D = context["dialogue_area"]
-	text_box.position = to_local(dialogue_area.global_position + Vector2(0, -50))
-	add_child(text_box)
+static func unsubscribe(function: Callable) -> void:
+	if not registered_functions.has(function): return
+	registered_functions.erase(function)
